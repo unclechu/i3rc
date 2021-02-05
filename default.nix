1154ebec6fdd97563ef2709b6b994ef9c5c869c9
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-02-05 03:27:16 +0200

    Add Nix support

---
+++ default.nix
@@ -0,0 +1,79 @@
+#
+# This module is supposed to be added to the imports of ‘configuration.nix’.
+#
+# For instance (in ‘configuration.nix’):
+#   { pkgs, ... }:
+#   {
+#     imports = [
+#       (import (pkgs.fetchFromGitHub {
+#         owner  = "unclechu";
+#         repo   = "i3rc";
+#         rev    = "0000000000000000000000000000000000000000";
+#         sha256 = "0000000000000000000000000000000000000000000000000000";
+#       }) { inherit pkgs; })
+#     ];
+#   }
+#
+let sources = import nix/sources.nix; in
+{ pkgs ? import sources.nixpkgs {}
+
+, configFile ? ./config
+
+, # A script that runs each time i3 just starts
+  # or restarts within the same session.
+  autostartScript ? null
+
+, # Script names to replace with custom paths to them
+  # (for instance with paths to somewhere in Nix store).
+  # This is an attribute set where the attribute name is the name of a script
+  # and attribute value is the new path to that script.
+  # See the asserts below for available script names for overriding.
+  scriptsPaths ? {}
+}:
+let
+  isFilePath = path:
+    builtins.isString path ||
+    (builtins.isPath path && pkgs.lib.pathIsRegularFile path);
+in
+
+assert isFilePath configFile;
+assert ! isNull autostartScript -> isFilePath autostartScript;
+assert builtins.isAttrs scriptsPaths;
+
+# Available scripts to override paths of.
+assert [] == pkgs.lib.subtractLists
+  [
+    "autostart.sh"
+    "input.sh"
+    "cursor-to-display.pl"
+    "gpaste-gui.pl"
+    "pamng.sh"
+    "screen-backlight.sh"
+  ]
+  (builtins.attrNames scriptsPaths);
+
+assert builtins.all isFilePath (builtins.attrValues scriptsPaths);
+
+let
+  patchArgs =
+    builtins.foldl'
+      (acc: from: {
+        from = acc.from ++ [ from                 ];
+        to   = acc.to   ++ [ scriptsPaths.${from} ];
+      })
+      { from = []; to = []; }
+      (builtins.attrNames scriptsPaths);
+
+  patchFn = builtins.replaceStrings patchArgs.from patchArgs.to;
+
+  config = pkgs.writeText "wenzels-i3-config-file" ''
+    ${patchFn (builtins.readFile configFile)}
+    ${if isNull autostartScript then "" else "exec_always ${autostartScript}"}
+  '';
+in
+{
+  services.xserver.windowManager.i3 = {
+    enable     = true;
+    configFile = config;
+  };
+}
